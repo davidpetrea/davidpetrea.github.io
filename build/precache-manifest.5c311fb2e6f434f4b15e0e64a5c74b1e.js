e719fe6b9620ac39f52b5e9c3744a7a06518e2b2
davidpetrea <52080760+davidpetrea@users.noreply.github.com>
2019-10-09 15:36:37 +0300

    Commit2

---
+++ build/precache-manifest.5c311fb2e6f434f4b15e0e64a5c74b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70c80dc377a8ad216be7a21c2df8e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dfaac3e3deb110ed2af",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8275a68893bfbf933259",
+    "url": "/static/js/2.2bd10b68.chunk.js"
+  },
+  {
+    "revision": "7dfaac3e3deb110ed2af",
+    "url": "/static/js/main.7ccad10c.chunk.js"
+  },
+  {
+    "revision": "27f78de38451a7b0dd47",
+    "url": "/static/js/runtime-main.8db32b03.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
